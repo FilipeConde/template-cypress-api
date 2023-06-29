e61ab34806f19cd0f5fb0d0d8d85500fcd1c449b
Filipe Conde <filipecondep@gmail.com>
2023-06-29 19:56:51 -0300

    Atualiza arquivos de suport (métodos de req HTTP, validação de schema e validação de valor na resposta.

--- cypress/support/validacoesCommands.js
+++ cypress/support/validacoesCommands.js
@@ -6,6 +6,14 @@ Cypress.Commands.add('validarMensagem', (body, mensagem) => {
   expect(Object.values(body)).to.contain(mensagem)
 })
 
+Cypress.Commands.add('validarValor', (body, objeto, valor) => {
+  let objectPath = body
+
+  if (objeto !== 'body') objectPath = eval(`body.${objeto}`)
+
+  expect(Object.values(objectPath)).to.contain(valor)
+})
+
 Cypress.Commands.add('validarStatusCode', (statusCode) => {
   cy.get('@RES_STATUS').then(resStatus => {
       expect(statusCode).equals(resStatus)
@@ -13,16 +21,19 @@ Cypress.Commands.add('validarStatusCode', (statusCode) => {
 })
 
 Cypress.Commands.add('validarSchema', (schema, res) => {
-  const validation = validate(res, schema, { required: true, nestedErrors: true });
+  const validation = validate(res, schema, {
+    required: true,
+    nestedErrors: true,
+  })
   let errors = ''
 
   if (!validation.valid) {
-    errors += validation.errors.map(err => {
+    errors += validation.errors.map((err) => {
       return `\n The element ${err.path} ${err.message}`
     })
     cy.log('SCHEMA VALIDATION EROR: ' + errors)
-    // throw new Error('SCHEMA VALIDATION ERROR: ' + errors);
-    // expect(validation.valid).to.be.true
+    throw new Error('SCHEMA VALIDATION ERROR: ' + errors)
+  } else {
+    expect(validation.valid).to.be.true
   }
-
 })
